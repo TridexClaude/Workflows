674461dfbf4e5445ace06b5c1734212aef4b2248
Claude Compere <claudecompere@Claudes-MacBook-Pro.local>
2016-07-20 22:20:35 +0100

    Added Static File Monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,12 @@ var jsSources       = [
 var sassSources     = [
     'components/sass/style.scss'
 ]
+var htmlSources     = [
+    'builds/development/*.html'
+]
+var jsonSources     = [
+    'builds/development/js/*.json'
+]
 
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
@@ -53,10 +59,22 @@ gulp.task('connect', function() {
     });
 });
 
+gulp.task('html', function() {
+    gulp.src(htmlSources)
+        .pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+    gulp.src(jsonSources)
+        .pipe(connect.reload())
+});
+
 gulp.task('watch', function() {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
+    gulp.watch(htmlSources, ['html']);
+    gulp.watch(jsonSources, ['json']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch', 'connect']);
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'html', 'json', 'watch']);
